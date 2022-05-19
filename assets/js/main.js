7c26cb5c1e2e9f6a2e22d9f48bd2f340d55c060d
StephanPagel <stephan.pagel84@gmail.com>
2022-05-19 17:00:34 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,136 @@
+//Lvl1_1
+
+// let getraenke = [
+//     "Coca-Cola",
+//     "Apfelsaft",
+//     "Pepsi",
+//     "Traubensaft",
+//     "Sprite",
+//     "Orangensaft",
+//     "Red Bull Energy Drink",
+//     "Fanta"
+// ]
+
+// let getraenkeOutput = document.getElementById = 'getraenke';
+
+// let myDrinks = () => {
+//     getraenke.sort();
+//     getraenke.forEach((a) => {
+//         console.log(a);
+//         document.write(`<p>${a}</p>`);
+//     })
+// }
+
+// myDrinks();
+
+//Lvl1_2
+
+// let upperDrinks = getraenke.map(a => a.toUpperCase());
+// console.log(upperDrinks);
+
+//Lvl1_3
+
+// let array = [
+//     18,
+//     16,
+//     80,
+//     51,
+//     47,
+//     38,
+//     95,
+//     42,
+//     68,
+//     61,
+//     34,
+//     51,
+//     20,
+//     17,
+//     56,
+//     31,
+//     100,
+//     6,
+//     5,
+//     30,
+//     74,
+//     97,
+//     28,
+//     99,
+//     91,
+//     27,
+//     73,
+//     12,
+//     92,
+//     6,
+//     27,
+//     71,
+//     26,
+//     15,
+//     78
+// ];
+
+// let doublesort = () => {
+//     return array.map(a => a * 2).sort((a, b) => a - b);
+// }
+
+// console.log(doublesort());
+
+//Lvl1_4
+
+// let fahrenheit = [0, 32, 45, 50, 75, 80, 99, 120];
+
+// let toCelsius = () => {
+//     return fahrenheit.map(a => ((a - 32) / 1.8).toFixed(0));
+// }
+
+// console.log(toCelsius());
+
+//Lvl1_5
+
+// let checkNumber = [
+//     18,
+//     16,
+//     80,
+//     51,
+//     47,
+//     38,
+//     95,
+//     42,
+//     68,
+//     61,
+//     34,
+//     51,
+//     20,
+//     17,
+//     56,
+//     31,
+//     100,
+//     6,
+//     5,
+//     30,
+//     74,
+//     97,
+//     28,
+//     99,
+//     91,
+//     27,
+//     73,
+//     12,
+//     92,
+//     6,
+//     27,
+//     71,
+//     26,
+//     15,
+//     78
+// ];
+
+// let multiplied = [];
+
+// checkNumber.forEach((a) => {
+//     if (a % 3 === 0) {
+//         a += 100;
+//     }
+//     multiplied.push(a);
+// });
+
+// console.log(multiplied);
